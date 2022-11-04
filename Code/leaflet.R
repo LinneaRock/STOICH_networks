8a8f38e2f018982eda303d701a05efec8d2797e6
Linnea Ann Rock <lrock1@uwyo.edu>
2022-11-04 11:39:42 -0600

    creating leaflet

---
+++ Code/leaflet.R
@@ -0,0 +1,21 @@
+
+library(tidyverse)
+library(leaflet)
+
+
+network <- leaflet() |>
+  addTiles() |>
+  addMarkers(lng=-105.617178,lat=40.055807, popup='Green Lake 4 outlet') |>
+  addMarkers(lng=-105.60542,lat=40.049773, popup='Green Lake 1') |>
+  addMarkers(lng=-105.611345,lat=40.04934, popup='Green Lake 2') |>
+  addMarkers(lng=-105.616125,lat=40.051527, popup='Green Lake 3') |>
+  addMarkers(lng=-105.607123,lat=40.047178, popup='Lake Albion inlet') |>
+  addMarkers(lng=-105.602094,lat=40.045151, popup='Lake Albion outlet') |>
+  addMarkers(lng=-105.5923,lat=40.0429, popup='Albion Camp - downstream') |>
+  addMarkers(lng=-105.628126,lat=40.053672, popup='Green Lake 5 outlet') |>
+  addMarkers(lng=-105.642890924836,lat=40.0505740259853, popup='Arikaree Cirque') |>
+  addMarkers(lng=-105.6367,lat=40.05195303, popup='Navajo outlet') |>
+  addMarkers(lng=-105.5578493,lat=40.01368597, popup='Outlfow of Saddle Stream')
+
+network
+
--- Data/format_dataset.R
+++ Data/format_dataset.R
@@ -1,3 +1,20 @@
 library(tidyverse)
 
-lakes <- read.csv("C:/Users/linne/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.10.3/glvwatsolu.dm.data.csv")
+lakes <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.10.3/glvwatsolu.dm.data.csv")
+
+albdis <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.102.17/albdisch.nc.data.csv")
+albwq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.103.14/albisolu.nc.data.csv")
+
+arikwq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.104.13/ariksolu.nc.data.csv")
+navdis <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.169.1/navdisch.nc.data.csv")
+
+gl4dis <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.105.16/gl4disch.nc.data.csv")
+gl4wq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.108.12/gre4solu.nc.data.csv")
+
+gl5wq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.109.12/gre5solu.nc.data.csv")
+gl5dis <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.170.1/gl5disch.nc.data.csv") 
+
+albinwq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.110.7/inlesolu.nc.data.csv")
+flumewq <- read.csv("C:/Users/lrock1/OneDrive/Desktop/Niwot_Data/knb-lter-nwt.162.1/flumesolu.nc.data.csv")
+
+
