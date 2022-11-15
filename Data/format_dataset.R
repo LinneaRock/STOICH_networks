d1b86c16cda979fb678780782383910fb9fa8ee0
LinneaRock <lrock@wisc.edu>
2022-11-14 17:41:18 -0700

    formatting datasets

--- Data/format_dataset.R
+++ Data/format_dataset.R
@@ -55,6 +55,11 @@ lakes_format <- lakes |>
          TOC_mgL = TOC,
          DOC_mgL = DOC) |>
   relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL))
+  # get rid of rows without any nutrient data
+lakes_format <- lakes_format |>
+  mutate(check = rowSums(lakes_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
 
  
 alb_camp_format <- left_join(albdis, albwq, by = "date") |>
@@ -94,7 +99,12 @@ alb_camp_format <- left_join(albdis, albwq, by = "date") |>
          IP_umolL = IP,
          TOC_mgL = TOC,
          DOC_mgL = DOC) |>
-  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL))
+  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL)) 
+  # get rid of rows without any nutrient data
+alb_camp_format <- alb_camp_format |>
+  mutate(check = rowSums(alb_camp_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
 
 nav_format <- navdis |>
   rename(arik_flow_site = local_site,
@@ -107,6 +117,7 @@ arik_format <- left_join(nav_format, arikwq |> mutate(date = as.Date(date)), by 
   relocate(site) |>
   mutate(year = year(date)) |>
   select(-LTER_site, -time, - acid, - alkal, -Trit, -time, -D_excess, -dD_sdev, -dDeut, -d18O_sdev, -d18O, -T_sdev, -POC, -cat_sum, -an_sum, -chg_bal) |>
+  mutate(across(c(4,6:31), as.numeric)) |>
   rename(SpC_uscm = cond,
          ANC_ueqL = ANC,
          H_ueqL = H.,
@@ -133,5 +144,188 @@ arik_format <- left_join(nav_format, arikwq |> mutate(date = as.Date(date)), by 
          TOC_mgL = TOC,
          DOC_mgL = DOC) |>
   relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL))
+  # get rid of rows without any nutrient data
+arik_format <- arik_format |>
+  mutate(check = rowSums(arik_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
+
+
+
+gl4_format <- left_join(gl4dis, gl4wq, by = "date") |>
+  rename(daily_dis_m3 = discharge,
+         mean_temp_C = temperature) |>
+  mutate(site = "GL4_OUTLET",
+         date = as.Date(date),
+         year = year(date),
+         year = as.factor(year),
+         depth_m = 0) |>
+  relocate(site) |>
+  select(-notes, -LTER_site.x, -local_site.x, -LTER_site.y, -local_site.y, -time, - acid, - alkal, -Trit, -time, -D_excess, -dD_sdev, -dDeut, -d18O_sdev, -d18O, -T_sdev, -POC, -cat_sum, -an_sum, -chg_bal) |>
+  mutate(across(c(3,4,6:31), as.numeric)) |>
+  mutate(mean_temp_C = ifelse(is.nan(mean_temp_C), NA, mean_temp_C)) |>
+  rename(SpC_uscm = cond,
+         ANC_ueqL = ANC,
+         H_ueqL = H.,
+         NH4_ueqL = NH4.,
+         Ca_ueqL = Ca..,
+         Mg_ueqL = Mg..,
+         Na_ueqL = Na.,
+         K_ueqL = K.,
+         Cl_ueqL = Cl.,
+         NO3_ueqL = NO3.,
+         SO4_ueqL = SO4..,
+         PO4_ueqL = PO4...,
+         Si_umolL = Si,
+         TN_umolL = TN,
+         TDN_umolL = TDN,
+         PN_umolL = PN,
+         DON_umolL = DON,
+         IN_ueqL = IN,
+         TP_umolL = TP,
+         TDP_umolL = TDP,
+         PP_umolL = PP,
+         DOP_umolL = DOP,
+         IP_umolL = IP,
+         TOC_mgL = TOC,
+         DOC_mgL = DOC) |>
+  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL)) 
+  # get rid of rows without any nutrient data
+gl4_format <- gl4_format |>
+  mutate(check = rowSums(gl4_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
+
+
+gl5_format <- left_join(gl5dis, gl5wq, by = "date") |>
+  rename(daily_dis_m3 = discharge) |>
+         #mean_temp_C = temperature) |> no temperature data
+  mutate(site = "GL5_OUTLET",
+         date = as.Date(date),
+         year = year(date),
+         year = as.factor(year),
+         depth_m = 0) |>
+  relocate(site) |>
+  select(-notes, -LTER_site.x, -local_site.x, -LTER_site.y, -local_site.y, -time, - acid, - alkal, -Trit, -time, -D_excess, -dD_sdev, -dDeut, -d18O_sdev, -d18O, -T_sdev, -POC, -cat_sum, -an_sum, -chg_bal) |>
+  mutate(across(c(3,5:30), as.numeric)) |>
+  #mutate(mean_temp_C = ifelse(is.nan(mean_temp_C), NA, mean_temp_C)) |>
+  rename(SpC_uscm = cond,
+         ANC_ueqL = ANC,
+         H_ueqL = H.,
+         NH4_ueqL = NH4.,
+         Ca_ueqL = Ca..,
+         Mg_ueqL = Mg..,
+         Na_ueqL = Na.,
+         K_ueqL = K.,
+         Cl_ueqL = Cl.,
+         NO3_ueqL = NO3.,
+         SO4_ueqL = SO4..,
+         PO4_ueqL = PO4...,
+         Si_umolL = Si,
+         TN_umolL = TN,
+         TDN_umolL = TDN,
+         PN_umolL = PN,
+         DON_umolL = DON,
+         IN_ueqL = IN,
+         TP_umolL = TP,
+         TDP_umolL = TDP,
+         PP_umolL = PP,
+         DOP_umolL = DOP,
+         IP_umolL = IP,
+         TOC_mgL = TOC,
+         DOC_mgL = DOC) |>
+  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL)) 
+  # get rid of rows without any nutrient data
+gl5_format <- gl5_format |>
+  mutate(check = rowSums(gl5_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
+
+
+albin_format <- albinwq |>
+  mutate(site = "ALB_INLET",
+         date = as.Date(date),
+         year = year(date),
+         year = as.factor(year),
+         depth_m = 0) |>
+  relocate(site) |>
+  select(-LTER_site, -local_site, -time, - acid, - alkal, -Trit, -time, -D_excess, -dD_sdev, -dDeut, -d18O_sdev, -d18O, -T_sdev, -POC, -cat_sum, -an_sum, -chg_bal) |>
+  mutate(across(c(4:29), as.numeric)) |>
+  #mutate(mean_temp_C = ifelse(is.nan(mean_temp_C), NA, mean_temp_C)) |>
+  rename(SpC_uscm = cond,
+         ANC_ueqL = ANC,
+         H_ueqL = H.,
+         NH4_ueqL = NH4.,
+         Ca_ueqL = Ca..,
+         Mg_ueqL = Mg..,
+         Na_ueqL = Na.,
+         K_ueqL = K.,
+         Cl_ueqL = Cl.,
+         NO3_ueqL = NO3.,
+         SO4_ueqL = SO4..,
+         PO4_ueqL = PO4...,
+         Si_umolL = Si,
+         TN_umolL = TN,
+         TDN_umolL = TDN,
+         PN_umolL = PN,
+         DON_umolL = DON,
+         IN_ueqL = IN,
+         TP_umolL = TP,
+         TDP_umolL = TDP,
+         PP_umolL = PP,
+         DOP_umolL = DOP,
+         IP_umolL = IP,
+         TOC_mgL = TOC,
+         DOC_mgL = DOC) |>
+  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL))
+  # get rid of rows without any nutrient data
+albin_format <- albin_format |>
+  mutate(check = rowSums(albin_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
+
+
+
+flume_format <- flumewq |>
+  mutate(site = "FLUME",
+         date = as.Date(date),
+         year = year(date),
+         year = as.factor(year),
+         depth_m = 0) |>
+  relocate(site) |>
+  select(-LTER_site, -local_site, -time, - acid, - alkal, -Trit, -time, -D_excess, -dD_sdev, -dDeut, -d18O_sdev, -d18O, -T_sdev, -POC, -cat_sum, -an_sum, -chg_bal) |>
+  mutate(across(c(4:29), as.numeric)) |>
+  #mutate(mean_temp_C = ifelse(is.nan(mean_temp_C), NA, mean_temp_C)) |>
+  rename(SpC_uscm = cond,
+         ANC_ueqL = ANC,
+         H_ueqL = H.,
+         NH4_ueqL = NH4.,
+         Ca_ueqL = Ca..,
+         Mg_ueqL = Mg..,
+         Na_ueqL = Na.,
+         K_ueqL = K.,
+         Cl_ueqL = Cl.,
+         NO3_ueqL = NO3.,
+         SO4_ueqL = SO4..,
+         PO4_ueqL = PO4...,
+         Si_umolL = Si,
+         TN_umolL = TN,
+         TDN_umolL = TDN,
+         PN_umolL = PN,
+         DON_umolL = DON,
+         IN_ueqL = IN,
+         TP_umolL = TP,
+         TDP_umolL = TDP,
+         PP_umolL = PP,
+         DOP_umolL = DOP,
+         IP_umolL = IP,
+         TOC_mgL = TOC,
+         DOC_mgL = DOC) |>
+  relocate(c(TOC_mgL, DOC_mgL, TN_umolL, TDN_umolL, PN_umolL, IN_ueqL, DON_umolL, NH4_ueqL, NO3_ueqL, TP_umolL,TDP_umolL, PP_umolL, IP_umolL, DOP_umolL, PO4_ueqL)) 
+
+flume_format <- flume_format |>
+  mutate(check = rowSums(flume_format[,c(1:15)], na.rm = TRUE)) |>
+  filter(check != 0) |>
+  select(-check)
 
          
