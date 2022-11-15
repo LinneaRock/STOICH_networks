6128b0478caf238c274b1ca56b7816738fd946bd
Linnea Ann Rock <lrock1@uwyo.edu>
2022-11-15 14:56:06 -0700

    timeseries

---
+++ Code/nutrient_timeseries.R
@@ -0,0 +1,65 @@
+
+library(tidyverse)
+
+gl_network <- read.csv("Data/greenlakes_network.csv") |>
+  select(-X) |>
+  mutate(date = as.Date(date))
+
+# not very much TOC data
+ggplot(gl_network, aes(date, TOC_mgL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year>1996), aes(date, DOC_mgL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year>1995 & year<2009), aes(date, TN_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year > 1995), aes(date, TDN_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year > 1995& year<2009), aes(date, PN_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year>1993), aes(date, IN_ueqL, color = site)) +
+  geom_point() +
+  geom_line()
+
+ggplot(gl_network|> filter(year > 1995), aes(date, DON_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+
+ggplot(gl_network |> filter(year>1995 & year<2009), aes(date, TP_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year > 1995), aes(date, TDP_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year > 1995& year<2012), aes(date, PP_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+
+ggplot(gl_network |> filter(year>1997), aes(date, IP_umolL, color = site)) +
+  geom_point() +
+  geom_line()
+
+ggplot(gl_network|> filter(year > 1995), aes(date, DOP_umolL, color = site)) +
+  geom_point() +
+  geom_line()
