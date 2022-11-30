61301305c47de62b3b4321e4636df45a7510a4bc
Linnea Ann Rock <lrock1@uwyo.edu>
2022-11-30 11:47:18 -0700

    working on figure of nutrient forms per site/season

---
+++ Code/figure.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(lubridate)
+
+sites <- read.csv("Data/sites.csv")
+gl_network <- read.csv("Data/greenlakes_network.csv") |>
+  select(-X) |>
+  left_join(sites)
+
+
+subset <- gl_network |>
+  select(3:17, network_position, eco_type, - NO3_ueqL, -NH4_ueqL, -PO4_ueqL) |>
+  mutate(mon = month(date), yr = year(date)) |> #and add seasons to the dataframe
+  mutate(season = case_when(mon %in% c(10,11,12) ~ "Oct-Dec",
+                            mon %in% c(1,2,3) ~ "Jan-Mar",
+                            mon %in% c(4,5,6)  ~ "Apr-Jun",
+                            mon %in% c(7,8,9) ~ "Jul-Sep")) |>
+  pivot_longer(1:10, names_to = 'parameter', values_to = 'concentration_umol_L') |>
+  drop_na() |>
+  mutate(parameter = sub('\\_.*', "", parameter)) |>
+  group_by(season, yr, site, network_position, eco_type, parameter) |>
+  summarise(meanconc_umolL = mean(concentration_umol_L)) |>
+  ungroup()
+         
+
+ggplot(subset, aes(yr, meanconc_umolL, color = network_position)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~parameter, scales = "free_y",ncol = 3) +
+  theme_bw()
+  
+
+
+
+
+forms_subset <- gl_network |>
+  select(3:17, network_position, eco_type, - NO3_ueqL, -NH4_ueqL, -PO4_ueqL) |>
+  drop_na()
