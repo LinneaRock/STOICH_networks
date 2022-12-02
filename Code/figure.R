540a52288e003c76a6eaece96bde450234c650bb
LinneaRock <lrock@wisc.edu>
2022-12-01 20:26:26 -0700

    bad figure

--- Code/figure.R
+++ Code/figure.R
@@ -30,6 +30,34 @@ ggplot(subset, aes(yr, meanconc_umolL, color = network_position, group = site)) 
   theme_bw()
   
 
+inorganic <- gl_network |>
+  select(site, network_position, date, eco_type, TP_umolL, IP_umolL, TN_umolL, IN_umolL) |>
+  drop_na() |>
+  mutate(percentIP = (IP_umolL/TP_umolL) * 100) |>
+  mutate(percentIN = (IN_umolL/TN_umolL) * 100) |>
+  filter(percentIN < 100,
+         percentIP < 100) |>
+  mutate(mon = month(date), yr = year(date)) |> #and add seasons to the dataframe
+  mutate(season = case_when(mon %in% c(10,11,12) ~ "Oct-Dec",
+                            mon %in% c(1,2,3) ~ "Jan-Mar",
+                            mon %in% c(4,5,6)  ~ "Apr-Jun",
+                            mon %in% c(7,8,9) ~ "Jul-Sep")) |>
+  #pivot_longer(cols=c('percentIN','percentIP'), values_to = 'percents', names_to = 'nutrient') |>
+  mutate(network_position=as.factor(network_position)) |>
+  mutate(season = factor(season, levels = c('Jan-Mar', 'Apr-Jun', 'Jul-Sep', 'Oct-Dec')))
+  
+
+library(ggridges)
+
+ggplot() +
+  geom_density_ridges(inorganic, mapping = aes(percentIP, network_position),
+                      alpha = 0.5, scale=1.5, rel_min_height=0.02, fill='#336a98') +
+  geom_density_ridges(inorganic, mapping = aes(percentIN, network_position),
+                      alpha = 0.5, scale=1.5, rel_min_height=0.02, fill='red4') +
+  theme_bw() +
+  labs(x='% inorganic of total nutrient', y='Network position') +
+  facet_grid(.~season)
+ggsave('densityplot.png', height=4.5, width = 6.5, units='in', dpi=1200)
 
 
 
