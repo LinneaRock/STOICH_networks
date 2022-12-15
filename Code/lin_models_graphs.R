0e4410aa4bed42663db42d904556b3afbc7a057c
LinneaRock <lrock@wisc.edu>
2022-12-15 13:26:38 -0700

    starting models

---
+++ Code/lin_models_graphs.R
@@ -0,0 +1,26 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Script to look at nutrients along the network
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+library(tidyverse)
+sites <- read.csv("Data/sites.csv")
+gl_network <- read.csv("Data/greenlakes_network.csv") |>
+  left_join(sites) |>
+  mutate(date = as.Date(date, format = '%m/%d/%Y'))  |>
+  mutate(season = factor(season, levels = c('Jan-Mar','Apr-Jun','Jul-Sep','Oct-Dec')))
+
+plot_log <- function(param, name) {
+  ggplot(gl_network, aes(network_position, param)) +
+    geom_point(aes(color = season)) +
+    scale_y_log10() +
+    geom_smooth() +
+    labs(y = name)
+  }
+
+plot_log(gl_network$TN_umol, 'TN_umolL')
+summary(lm(log10(TN_umolL)~network_position, gl_network |> filter(TN_umolL > 0 & !is.na(TN_umolL))))  
+
+
+
+  
