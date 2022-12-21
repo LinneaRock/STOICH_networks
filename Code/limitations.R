08aa95ecc4bcd2089191d265cdaa40fbc54be42d
LinneaRock <lrock@wisc.edu>
2022-12-21 14:38:50 -0700

    limitations -- all P limited basically.
    played aroudn with maps. needs more work

---
+++ Code/limitations.R
@@ -0,0 +1,55 @@
+#::::::::::::::::::::
+# Nutrient limitation
+#::::::::::::::::::::   
+
+library(tidyverse)
+
+sites <- read.csv("Data/sites.csv") |>
+  drop_na(long)
+gl_network <- read.csv("Data/greenlakes_network.csv") |>
+  left_join(sites) |>
+  mutate(date = as.Date(date, format = '%m/%d/%Y'))  |>
+  mutate(season = factor(season, levels = c('Jan-Mar','Apr-Jun','Jul-Sep','Oct-Dec'))) |>
+  mutate(network_position = as.factor(network_position))
+
+
+
+
+# DIN:TP limitation ####
+  ggplot(gl_network) +
+  geom_point(aes(log10(TP_umolL), log10(IN_umolL/TP_umolL)), size = 2.5, shape = 21, alpha = 0.5) +
+  geom_abline(slope = 0, intercept = log10(3.4), linetype = "dashed") + # bergstrom P limitation line
+  geom_abline(slope = 0, intercept = log10(1.5), linetype = "dashed") +  # bergstrom N limitation line
+  #geom_abline(slope = 0, intercept = log(16, base = 10), color = "red4") + # redfield
+  theme_minimal() +
+  labs(y = "Log DIN:TP", x = "Log TP") +
+  #annotate('text', label = 'Redfield 16:1 line', x = 0.5, y = 1.1, hjust = 0, size = 2, color = "red4") +
+  annotate('text', label = 'Predicted N limitation below dashed line \n (Bergström, 2010)', x = -1, y = 0.1, hjust = 0, size = 2) +
+  annotate('text', label = 'Predicted P limitation above dashed line \n (Bergström, 2010)', x = 0.25, y = 0.5, hjust = 0, size = 2) 
+  
+
+# TN:TP limitation ####
+ggplot(gl_network) +
+  geom_point(aes(log10(TP_umolL), log10(TN_umolL/TP_umolL)), size = 2.5, shape = 21, alpha = 0.5) +
+  geom_abline(slope = 0, intercept = log10(41), linetype = "dashed") + # bergstrom P limitation line
+  geom_abline(slope = 0, intercept = log10(19), linetype = "dashed") +  # bergstrom N limitation line
+  geom_abline(slope = 0, intercept = log(16, base = 10), color = "red4") + # redfield
+  theme_minimal() +
+  labs(y = "Log TN:TP", x = "Log TP") +
+  annotate('text', label = 'Redfield 16:1 line', x = 0.5, y = 1.1, hjust = 0, size = 2, color = "red4") +
+  annotate('text', label = 'Predicted N limitation\n below dashed line \n (Bergström, 2010)', x = -1, y = 1.0, hjust = 0, size = 2) +
+  annotate('text', label = 'Predicted P limitation\n above dashed line \n (Bergström, 2010)', x = 0.25, y = 1.75, hjust = 0, size = 2)
+
+
+# DIN:DIP limitation ####
+ggplot(gl_network) +
+  geom_point(aes(log10(IP_umolL), log10((IN_umolL/IP_umolL))), size = 2.5, shape = 21, alpha = 0.5) +
+  geom_abline(slope = 0, intercept = log10(100), linetype = "dashed") + # Keck and Lepori P limitation line
+  geom_abline(slope = 0, intercept = log(1), linetype = "dashed") +  # keck and lepori N limitation line
+  geom_abline(slope = 0, intercept = log(16, base = 10), color = "red4") + # redfield
+  theme_minimal() +
+  labs(y = "Log TN:TP", x = "Log TP") +
+  annotate('text', label = 'Redfield 16:1 line', x = -0.5, y = 1.1, hjust = 0, size = 2, color = "red4") +
+  annotate('text', label = 'Predicted N limitation below dashed line \n (Keck & Lepori, 2012)', x = -2, y = -0.1, hjust = 0, size = 2) +
+  annotate('text', label = 'Predicted P limitation\n above dashed line \n (Keck & Lepori, 2012)', x = -2.5, y = 2, hjust = 0, size = 2)
+
--- Code/map_plots.R
+++ Code/map_plots.R
@@ -7,7 +7,8 @@ library(tidyverse)
 library(sf)
 library(ggspatial)
 
-sites <- read.csv("Data/sites.csv")
+sites <- read.csv("Data/sites.csv") |>
+  drop_na(long)
 gl_network <- read.csv("Data/greenlakes_network.csv") |>
   left_join(sites) |>
   mutate(date = as.Date(date, format = '%m/%d/%Y'))  |>
@@ -15,16 +16,27 @@ gl_network <- read.csv("Data/greenlakes_network.csv") |>
   mutate(network_position = as.factor(network_position))
 
 
-plots <- st_read('Data/lter_plots_09_05_2022/lter_plots.shp')
+#plots <- st_read('Data/lter_plots_09_05_2022/lter_plots.shp')
 
 # # Background maps 
-# esri_land <- paste0('https://services.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/tile/${z}/${y}/${x}.jpeg')
-# basemap <- paste0('https://tiles.wmflabs.org/osm-no-labels/${z}/${x}/${y}.png')
-# 
-# ggplot(plots) +
-#   annotation_map_tile(type = esri_land, zoom = 12) +
-#   geom_sf(aes(fill = PROJECT))
+esri_land <- paste0('https://services.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/tile/${z}/${y}/${x}.jpeg')
+#basemap <- paste0('https://tiles.wmflabs.org/osm-no-labels/${z}/${x}/${y}.png')
+topo_map  <- paste0('https://services.arcgisonline.com/arcgis/rest/services/World_Terrain_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
+
+
+sites.sf = st_as_sf(sites, coords = c("long", "lat"), 
+                      crs = 4326)
+
+
+ggplot(sites.sf |> filter(site != 'FLUME')) +
+  annotation_map_tile(type = topo, zoom = 5) +
+  geom_sf() 
 
 
 
-remotes::install_github("DOI-USGS/nhdplusTools")
+library(nhdplusTools)
+
+
+bbox <- sf::st_bbox(c(xmin = -105.644739, ymin = 40.040162, xmax = -105.588581, ymax = 40.058095),
+                    crs = "+proj=longlat +datum=WGS84 +no_defs")
+plot_nhdplus(bbox = bbox)
