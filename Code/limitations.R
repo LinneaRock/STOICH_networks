c378a1f1c6017e30d5421cfb6ebbbc3c47df267f
LinneaRock <lrock@wisc.edu>
2022-12-22 12:47:21 -0700

    medians

--- Code/limitations.R
+++ Code/limitations.R
@@ -20,7 +20,7 @@ medians <- gl_network |>
          yearlymedTN_umolL = median(TN_umolL, na.rm = TRUE),
          yearlymedIP_umolL = median(IP_umolL, na.rm = TRUE)) |>
   ungroup() |>
-  select(site, season, `year(date)`, yearlymedTP_umolL, yearlymedIP_umolL, yearlymedIN_umolL, yearlymedTN_umolL) |>
+  select(site, season, `year(date)`,eco_type, yearlymedTP_umolL, yearlymedIP_umolL, yearlymedIN_umolL, yearlymedTN_umolL) |>
   distinct() |>
   group_by(site, season) |>
   mutate(MEDTP_umolL = median(yearlymedTP_umolL, na.rm = TRUE),
@@ -28,21 +28,19 @@ medians <- gl_network |>
          MEDIN_umolL = median(yearlymedIN_umolL, na.rm = TRUE),
          MEDIP_umolL = median(yearlymedIP_umolL, na.rm = TRUE)) |>
   ungroup() |>
-  select(site, season, MEDTP_umolL, MEDTN_umolL, MEDIP_umolL, MEDIN_umolL) |>
+  select(site, season, eco_type, MEDTP_umolL, MEDTN_umolL, MEDIP_umolL, MEDIN_umolL) |>
   distinct()
   
 
 
 # DIN:TP limitation ####
 ggplot() +
-  geom_point(gl_network, mapping = aes(log10(TP_umolL), log10(IN_umolL/TP_umolL), fill = season), shape = 21, alpha = 0.8) +
-  geom_point(medians, mapping = aes(log10(MEDTP_umolL), log10(MEDIN_umolL/MEDTP_umolL), fill = season), color ='black', size = 3, shape = 21) +
+  geom_point(gl_network, mapping = aes(log10(TP_umolL), log10(IN_umolL/TP_umolL), fill = season), shape = 21, alpha = 0.2) +
+  geom_point(medians, mapping = aes(log10(MEDTP_umolL), log10(MEDIN_umolL/MEDTP_umolL), color = season, shape = eco_type), size = 3) +
   geom_abline(slope = 0, intercept = log10(3.4), linetype = "dashed") + # bergstrom P limitation line
   geom_abline(slope = 0, intercept = log10(1.5), linetype = "dashed") +  # bergstrom N limitation line
-  #geom_abline(slope = 0, intercept = log(16, base = 10), color = "red4") + # redfield
   theme_minimal() +
   labs(y = "Log DIN:TP", x = "Log TP") +
-  #annotate('text', label = 'Redfield 16:1 line', x = 0.5, y = 1.1, hjust = 0, size = 2, color = "red4") +
   annotate('text', label = 'Predicted N limitation below dashed line \n (Bergström, 2010)', x = -1, y = 0.1, hjust = 0, size = 2) +
   annotate('text', label = 'Predicted P limitation above dashed line \n (Bergström, 2010)', x = 0.25, y = 0.5, hjust = 0, size = 2) 
   
