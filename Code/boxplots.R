00dca5c4b0f2e9d7c31e49febfe9592b8c3537c5
LinneaRock <lrock@wisc.edu>
2023-01-06 16:40:20 -0700

    working on comparisons

--- Code/boxplots.R
+++ Code/boxplots.R
@@ -9,7 +9,8 @@ library(ggpubr)
 library(multcompView)
 
 
-sites <- read.csv("Data/sites.csv")
+sites <- read.csv("Data/sites.csv") |>
+  mutate(network_position = factor(network_position, levels = c('1','2','3','4', '5', '6', '7','8','9','10','11','12','12a','13','14','15','16')))
 gl_network <- read.csv("Data/greenlakes_network.csv") |>
   left_join(sites) |>
   mutate(date = as.Date(date, format = '%m/%d/%Y'))  |>
@@ -50,12 +51,13 @@ ggplot(ions, aes(eco_type, result)) +
         panel.grid.major.x = element_blank(),
         text=element_text(family='serif'),
         axis.text=element_text(color='black')) +
-  scale_color_viridis_c('Network position',direction = -1) +
+  scale_color_viridis_d('Network position',direction = -1) +
   facet_wrap(.~param, scales = 'free_y') +
   labs(x = '', y = '') +
   stat_compare_means(fontface='bold',label = 'p.signif',comparisons = list(c('glacier','lake'), c('lake','stream'), c('glacier','stream'))) +
   # ^^ Kruskal-Wallis test --- y.label.npc does not work ???!!!!!! 
   scale_y_continuous(expand = expansion(mult = c(0.05, 0.1)))+
+  guides(color = guide_legend(ncol = 3)) +
   theme(legend.position = c(1, 0), legend.justification = c(1, 0),
         legend.key.size = unit(0.5, 'cm'), #change legend key size
         legend.key.height = unit(0.5, 'cm'), #change legend key height
@@ -78,12 +80,13 @@ ggplot(nuts, aes(eco_type, result)) +
         panel.grid.major.x = element_blank(),
         text=element_text(family='serif'),
         axis.text=element_text(color='black')) +
-  scale_color_viridis_c('Network position',direction = -1) +
+  scale_color_viridis_d('Network position',direction = -1) +
   facet_wrap(.~param, scales = 'free_y') +
   labs(x = '', y = '') +
   stat_compare_means(fontface='bold',label = 'p.signif',comparisons = list(c('glacier','lake'), c('lake','stream'), c('glacier','stream'))) +
   # ^^ Kruskal-Wallis test --- y.label.npc does not work ???!!!!!! 
   scale_y_continuous(expand = expansion(mult = c(0.05, 0.1)))+
+  guides(color = guide_legend(ncol = 3)) +
   theme(legend.position = c(1, 0), legend.justification = c(1, 0),
         legend.key.size = unit(0.5, 'cm'), #change legend key size
         legend.key.height = unit(0.5, 'cm'), #change legend key height
@@ -123,18 +126,18 @@ ggplot(ions, mapping = aes(network_position, result, group = network_position)) 
         legend.key.height = unit(0.5, 'cm'), #change legend key height
         legend.key.width = unit(1, 'cm'), #change legend key width
         legend.title = element_text(size=12), #change legend title font size
-        legend.text = element_text(size=10)) #change legend text font size
-  
-ggsave('Figures/boxplots/position_params.png', height = 6.5, width = 8.5, units = 'in', dpi = 1200)
+        legend.text = element_text(size=10),  #change legend text font size
+        axis.text.x = element_text(angle=45, vjust=1,hjust=1))
+ggsave('Figures/boxplots/position_params.png', height = 8.5, width = 10.5, units = 'in', dpi = 1200)
 
 # get significance to add to figure using geom_text -- actually way too much on these plots
 list_params <- as.vector(nuts |> select(param) |> distinct())[['param']]
 sig.letters <- data.frame(NA)
 for(i in list_params) {
-  h <- aov(result~as.factor(network_position), nuts|>filter(param==i))
+  h <- aov(result~network_position, nuts|>filter(param==i))
   tukey <- TukeyHSD(h)
   cld <- multcompLetters4(h, tukey)
-  cld2 <- data.frame(letters = cld$`as.factor(network_position)`$Letters) |>
+  cld2 <- data.frame(letters = cld$network_position$Letters) |>
     mutate(param = i)
   cld2$network_position <- rownames(cld2)
   sig.letters <- sig.letters |> bind_rows(cld2)
@@ -142,8 +145,7 @@ for(i in list_params) {
 
 sig.letters <- sig.letters |>
   drop_na(letters) |>
-  select(-NA.) |>
-  mutate(network_position = as.numeric(network_position))
+  select(-NA.) 
 
 means <- left_join(nuts, sig.letters) |>
   group_by(letters, param, network_position) |>
@@ -173,19 +175,18 @@ ggplot(nuts, aes(network_position, result, group = network_position)) +
         legend.key.height = unit(0.5, 'cm'), #change legend key height
         legend.key.width = unit(1, 'cm'), #change legend key width
         legend.title = element_text(size=12), #change legend title font size
-        legend.text = element_text(size=10)) #change legend text font size
-
-
-ggsave('Figures/boxplots/position_nuts.png', height = 6.5, width = 8.5, units = 'in', dpi = 1200)
+        legend.text = element_text(size=10),  #change legend text font size
+        axis.text.x = element_text(angle=45, vjust=1,hjust=1))
+ggsave('Figures/boxplots/position_nuts.png', height =8.5, width = 10.5, units = 'in', dpi = 1200)
 
 # get significance to add to figure using geom_text -- actually way too much on these plots
 list_params <- as.vector(ions |> select(param) |> distinct())[['param']]
 sig.letters <- data.frame(NA)
 for(i in list_params) {
-  h <- aov(result~as.factor(network_position), ions|>filter(param==i))
+  h <- aov(result~network_position, ions|>filter(param==i))
   tukey <- TukeyHSD(h)
   cld <- multcompLetters4(h, tukey)
-  cld2 <- data.frame(letters = cld$`as.factor(network_position)`$Letters) |>
+  cld2 <- data.frame(letters = cld$network_position$Letters) |>
     mutate(param = i)
   cld2$network_position <- rownames(cld2)
   sig.letters <- sig.letters |> bind_rows(cld2)
@@ -193,8 +194,7 @@ for(i in list_params) {
 
 sig.letters <- sig.letters |>
   drop_na(letters) |>
-  select(-NA.) |>
-  mutate(network_position = as.numeric(network_position))
+  select(-NA.) 
 
 means <- left_join(ions, sig.letters) |>
   group_by(letters, param, network_position) |>
@@ -208,7 +208,8 @@ means <- left_join(ions, sig.letters) |>
 # plotting by season ####
 ## plot non-nutrients by season ####
 ggplot(ions, aes(season, result, group = season)) +
-  geom_jitter(aes(color = eco_type, shape = depth_m), shape = 16, size =2, alpha = 0.2, position=position_jitter(0.3)) +
+  geom_jitter(aes(color = eco_type, shape = depth_m), 
+              shape = 16, size =2, alpha = 0.2, position=position_jitter(0.3)) +
   geom_violin(alpha = 0.2) +
   stat_summary(geom = 'point', fun.y = 'mean', fill = 'black', color = 'white', size = 1.5, shape = 24) +
   theme_bw(base_size = 15) +
@@ -219,7 +220,7 @@ ggplot(ions, aes(season, result, group = season)) +
         text=element_text(family='serif'),
         axis.text=element_text(color='black'),
         axis.text.x = element_text(angle=45, vjust=1,hjust=1)) +
-  scale_color_viridis_d('Network position',direction = -1) +
+  scale_color_viridis_d('',direction = -1) +
   facet_wrap(.~param, scales = 'free_y') +
   labs(x = '', y = '') +
   geom_text(means, mapping = aes(season, max.result + 1, label = letters), hjust = -0.5) +
@@ -274,7 +275,7 @@ ggplot(nuts, aes(season, result, group = season)) +
         text=element_text(family='serif'),
         axis.text=element_text(color='black'),
         axis.text.x = element_text(angle=45, vjust=1,hjust=1)) +
-  scale_color_viridis_d('Network position',direction = -1) +
+  scale_color_viridis_d('',direction = -1) +
   facet_wrap(.~param, scales = 'free_y') +
   labs(x = '', y = '') +
   geom_text(means, mapping = aes(season, max.result + 1, label = letters), hjust = -0.5) +
--- Code/tables_comparisons.R
+++ Code/tables_comparisons.R
@@ -14,6 +14,8 @@ gl_network <- read.csv("Data/greenlakes_network.csv") |>
   mutate(date = as.Date(date, format = '%m/%d/%Y'))  |>
   mutate(season = factor(season, levels = c('Jan-Mar','Apr-Jun','Jul-Sep','Oct-Dec')))
 
+
+
 # subset and format datasets for plotting ####
 ions <- gl_network |>
   select(site, network_position, eco_type, date, season, depth_m, lat, long, 21:31) |>
@@ -31,69 +33,116 @@ nuts <- gl_network |>
   filter(depth_m <=3 | is.na(depth_m),  # just look at photic zone
          site != 'FLUME')
 
+stoich <- gl_network |>
+  select(site, network_position, eco_type, date, season, depth_m, lat, long, 3:7, 10:14) |>
+  mutate(tn.tp = TN_umolL/TP_umolL,
+         don.dop = DON_umolL/DOP_umolL,
+         tdn.tdp = TDN_umolL/TDP_umolL,
+         pn.pp = PN_umolL/PP_umolL,
+         in.ip = IN_umolL/IP_umolL) |>
+  select(1:8, 19:23) |>
+  pivot_longer(9:13, names_to = 'param', values_to = 'result') |>
+  drop_na(network_position)  |> # get rid of weird sites with no locational information
+  drop_na(result) |>
+  filter(depth_m <=3 | is.na(depth_m),  # just look at photic zone
+         site != 'FLUME',
+         is.finite(result))
+
+
+
+
+
 # get basic stats for each site, parameter ####
-tmp <- ions |>
-  group_by(site, eco_type, network_position, param, season) |>
-  summarise(mean = mean(result),
-            median = median(result),
-            min = min(result),
-            max = max(result),
-            SD = sd(result)) |>
-  ungroup() |>
-  mutate(CV = (SD/mean) * 100) 
+get_stats <- function(data) {
+  
+  tmp <- data |>
+    group_by(site, eco_type, network_position, param, season) |>
+    summarise(mean = mean(result),
+              median = median(result),
+              min = min(result),
+              max = max(result),
+              SD = sd(result),
+              n = n()) |>
+    ungroup() |>
+    mutate(CV = (SD/mean) * 100) 
+  
+  tmp2 <- data |>
+    group_by(site, eco_type, network_position, param) |>
+    summarise(mean = mean(result),
+              median = median(result),
+              min = min(result),
+              max = max(result),
+              SD = sd(result),
+              n = n()) |>
+    ungroup() |>
+    mutate(CV = (SD/mean) * 100,
+           season = 'All data') 
+  
+  df <- rbind(tmp, tmp2)
+  
+  rm(tmp)
+  rm(tmp2)
+  return(df)
+  
+}
 
-tmp2 <- ions |>
-  group_by(site, eco_type, network_position, param) |>
-  summarise(mean = mean(result),
-            median = median(result),
-            min = min(result),
-            max = max(result),
-            SD = sd(result)) |>
-  ungroup() |>
-  mutate(CV = (SD/mean) * 100,
-         season = 'All data') 
-
-stats_ions <- rbind(tmp, tmp2)
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-tmp <- nuts |>
-  group_by(site, eco_type, network_position, param, season) |>
-  summarise(mean = mean(result),
-            median = median(result),
-            min = min(result),
-            max = max(result),
-            SD = sd(result)) |>
-  ungroup() |>
-  mutate(CV = (SD/mean) * 100) 
-
-tmp2 <- nuts |>
-  group_by(site, eco_type, network_position, param) |>
-  summarise(mean = mean(result),
-            median = median(result),
-            min = min(result),
-            max = max(result),
-            SD = sd(result)) |>
-  ungroup() |>
-  mutate(CV = (SD/mean) * 100,
-         season = 'All data') 
-
-stats_nuts <- rbind(tmp, tmp2)
-
-rm(tmp)
-rm(tmp2)
+stats_ions <- get_stats(ions)
+stats_nuts <- get_stats(nuts)
+stats_stoich <- get_stats(stoich)
 
 
-ggplot(stats_nuts |> filter(season != 'All data')) +
-  geom_line(aes(network_position, mean, group = season, color = season), alpha = 0.5, linewidth = 1) +
-  geom_point(aes(network_position, mean, group = season, fill = season), 
-             color = "black", pch = 21, size = 2, position=position_dodge(width=0.1)) +
+# plotting by network position and season ####
+plot_season <- function(data) {
+  ggplot(data |> filter(season != 'All data')) +
+    geom_line(aes(network_position, mean, group = season, color = season), alpha = 0.5, linewidth = 1) +
+    geom_point(aes(network_position, mean, group = season, fill = season), 
+               color = "black", pch = 21, size = 2, position=position_dodge(width=0.1)) +
     geom_errorbar(aes(network_position, mean, ymin = mean-SD, ymax = mean+SD, color = season),
                   position=position_dodge(width=0.1), linetype = 'dashed')  +
-  scale_color_OkabeIto() +
-  scale_fill_OkabeIto() +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank()) +
-  facet_wrap(~param, ncol=3, scales = 'free_y')
+    scale_color_OkabeIto() +
+    scale_fill_OkabeIto() +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) +
+    facet_wrap(~param, ncol=3, scales = 'free_y')
+}
 
+
+plot_season(stats_nuts)
+ggsave("Figures/network_nuts_ts_seasonal.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+plot_season(stats_ions)
+ggsave("Figures/network_ions_ts_seasonal.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+plot_season(stats_stoich) +
+  ylim(0,NA)
+ggsave("Figures/network_stoich_ts_seasonal.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+
+
+# plotting by network position all data ####
+plot_alldat <- function(data) {
+  ggplot(data |> filter(season == 'All data')) +
+    geom_point(aes(network_position, mean), 
+               color = "black", pch = 21, size = 2) +
+    geom_errorbar(aes(network_position, mean, ymin = mean-SD, ymax = mean+SD), linetype = 'dashed')  +
+    geom_line(aes(network_position, mean, group = 1)) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) +
+    facet_wrap(~param, ncol=3, scales = 'free_y')
+}
+
+plot_alldat(stats_nuts)
+ggsave("Figures/network_nuts_ts_all.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+plot_alldat(stats_ions)
+ggsave("Figures/network_ions_ts_all.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+plot_alldat(stats_stoich) +
+  ylim(0,NA)
+ggsave("Figures/network_stoich_ts_all.png", width = 10.5, height = 8.5, units = "in", dpi = 500)
+
+
+
+# compare lakes vs rivers vs glacier for all parameters 
