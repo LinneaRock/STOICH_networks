6b71d46ad83d9ccbce65a65670902ac89ec058c2
LinneaRock <lrock@wisc.edu>
2023-04-15 14:34:25 -0600

    trying to set up function

--- Code/synchrony.R
+++ Code/synchrony.R
@@ -1,7 +1,12 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Synchrony along the network - script looks at Pearson's correlations 
+# among all network positions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-
+# call data and packages ####
 source('Data/CALL_DATA_PACKAGES.R') 
 
+# prepare data ####
 nuts.setup <- nuts |>
   mutate(year=year(date)) |>
   select(-site, -depth_m, -date) |>
@@ -10,12 +15,49 @@ nuts.setup <- nuts |>
   ungroup() |>
   pivot_wider(names_from='param', values_from='med') |>
   select(-NH4_ueqL, - NO3_ueqL, -DOC_mgL) |>
-  filter(eco_type != 'glacier')
+  filter(eco_type != 'glacier') |>
+  group_by(network_position) |>
+  add_count()|>
+  ungroup()
 
-TN_corr_dat <- nuts.setup |>
-  select(1:4, TN_umolL) |>
+
+
+synchrony_fun <- function(setup.dat, variable, quoVariable) {
+
+corr_dat <- setup.dat |>
+  select(1:4, variable) |>
   drop_na() |>
-  group_by(network_position, eco_type, season) |>
-  summarise(mean_TN = mean(TN_umolL)) |>
+  group_by(network_position) |>
+  add_count() |>
   ungroup() |>
-  filter(season=='Jul-Sep')
+  filter(n>2) |>
+  unite(col = position, network_position, eco_type, sep = "_") |>
+  select(-n) |>
+  pivot_wider(id_cols = c('season', 'year'), 
+              names_from = 'position', values_from = quoVariable)  |>
+  select(-season, - year) |>
+  scale()
+# 
+# ggcorr(corr_dat, method=c('pairwise.complete.obs','pearson'))
+# check <- as.data.frame(cor(corr_dat, use='pairwise')) |>
+#   rownames_to_column() |>
+#   mutate(param = quoVariable)
+
+
+corr <- cor(corr_dat, use='pairwise')
+p.mat <-cor_pmat(corr)
+
+
+ggcorrplot(corr,
+           hc.order = FALSE, type = "lower",
+           outline.color = "white", p.mat = p.mat, insig = "blank", lab = TRUE,
+           ggtheme=ggplot2::theme_dark())
+ggsave(paste0('Figures/synchrony/', quoVariable, '.png'), 
+       height=4.25, width=6.25, units = 'in', dpi=1200)
+
+return(c(corr, p.mat))
+
+}
+
+
+synchrony_fun(nuts.setup, nuts.setup$TN_umolL, 'TN_umolL')
